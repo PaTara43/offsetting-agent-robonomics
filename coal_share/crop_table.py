22c61f771d5e6b495f439a16bab54b8f304c3811
PaTara43 <p040399@outlook.com>
2022-09-01 23:25:51 +0300

    update coal_share

--- coal_share/crop_table.py
+++ coal_share/crop_table.py
@@ -6,7 +6,10 @@ Crop table to reduce previous years. Source for table: https://ourworldindata.or
 import csv
 
 WORLD_2021_SHARE = 35.9863166809082
-prev_year = 0
+first_row = ['Afghanistan', 'AFG', '2001', '0']
+last_row = ['Zimbabwe', '30.963096618652344']
+
+prev_row = first_row
 
 
 with open('share-electricity-coal-01-09-2022.csv', 'r') as f_r:
@@ -16,14 +19,13 @@ with open('share-electricity-coal-01-09-2022.csv', 'r') as f_r:
         csv_cropped = csv.writer(f_w, delimiter=',')
 
         for row in csv_orig:
-            if int(row[2]) < prev_year:
-                if float(row[3]) == 0.0:
-                    cropped_row = [row[0], WORLD_2021_SHARE]
+            if row[0] != prev_row[0]:
+                if float(prev_row[3]) == 0.0:
+                    cropped_row = [prev_row[0], WORLD_2021_SHARE]
                 else:
-                    cropped_row = [row[0], float(row[3])]
+                    cropped_row = [prev_row[0], float(prev_row[3])]
                 csv_cropped.writerow(cropped_row)
 
-            prev_year = int(row[2])
+            prev_row = row
 
-
-
+        csv_cropped.writerow(last_row)
