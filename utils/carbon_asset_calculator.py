36f4ffdbfc6f2de6eba27dae60fea9315653a30d
PaTara43 <p040399@outlook.com>
2022-09-03 17:45:26 +0300

    add coal coefficient calculation

--- utils/carbon_asset_calculator.py
+++ utils/carbon_asset_calculator.py
@@ -3,12 +3,15 @@ Perform various carbon asset burning process calculations.
 
 """
 
+import csv
 import typing as tp
 
-from logging import getLogger
 from datetime import date
+from geopy.geocoders import Nominatim
+from logging import getLogger
 
-from db_interaction import sql_query
+from constants import COAL_SHARE_TABLE_PATH, WORLD_COAL_SHARE_COEFFICIENT
+from db_utils import sql_query
 
 logger = getLogger(__name__)
 
@@ -60,13 +63,37 @@ def get_kwt_to_burn(address: str, kwt_current: float) -> float:
 def get_tokens_to_burn(kwt: float, geo: str) -> float:
     """
     Get an amount of carbon assets to burn based on a number of kWt*h burnt and country of residence.
+        Source:
+        - Coal Share by countries: https://ourworldindata.org/grapher/share-electricity-coal.
 
-    :param kwt:
-    :param geo:
+    :param kwt: Number of kWt*h to compensate.
+    :param geo: Coordinates of the household.
 
-    :return:
+    :return: Number of carbon assets to burn.
 
     """
+    coal_share_coefficient: float = WORLD_COAL_SHARE_COEFFICIENT
 
+    logger.info(f"Getting country by geo: {geo}.")
+    geolocator = Nominatim(user_agent="geoapiExercises")
+    location = geolocator.reverse(geo, language="en")
+    if location:
+        country: str = location.raw['address']["country"]
+        logger.info(f"Country based on geo: {country}.")
 
-    return kwt
+        logger.info(f"Getting coal share coefficient for {country}.")
+        with open(COAL_SHARE_TABLE_PATH, 'r') as coal_share:
+            coal_share_csv = csv.reader(coal_share)
+            for row in coal_share_csv:
+                if row[0] == country:
+                    coal_share_coefficient = float(row[1])
+                    break
+        logger.info(f"Coal share coefficient for {country}: {coal_share_coefficient}.")
+
+    else:
+        logger.info(f"No country was determined. Using global coefficient: {WORLD_COAL_SHARE_COEFFICIENT}.")
+
+    kwt_coal: float = kwt*coal_share_coefficient/100
+    logger.info(f"Number of coal-powered kWt*h burnt: {kwt_coal}")
+
+    return kwt_coal
