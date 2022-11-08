13fc643041aa6ecfd4c8baf8327b3172492e19c8
PaTara43 <p040399@outlook.com>
2022-11-08 19:35:59 +0300

    comment update

--- main.py
+++ main.py
@@ -13,6 +13,5 @@ logger = logging.getLogger(__name__)
 if __name__ == "__main__":
     seed = os.getenv("OFFSETTING_AGENT_SEED")
     tokens_to_burn: float = get_tokens_to_burn(geo="59.934280, 30.335099", kwh=5.0)
-    # These are kwh produced by the country power stations, coefficients used to calculate coal share and tons of CO2
-    # emitted
+    # These are the kwh consumed from a state power system. Coefficients used to calculate mass of CO2 emitted.
     tr_hash: str = burn_carbon_asset(seed=seed, tokens_to_burn=tokens_to_burn)
