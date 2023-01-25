fd82df5eb90597fa7c1db130f6984f227432257f
PaTara <p040399@outlook.com>
2023-01-25 19:02:35 +0300

    0.4.0 message format update

--- worker.py
+++ worker.py
@@ -60,7 +60,7 @@ def callback_new_liability(data):
             logger.info(f"Reported liability {data[0]} at {report_tr_hash}")
             pubsub_send(
                 topic=LIABILITY_REPORT_TOPIC,
-                data=json.dumps({"address": data[3], "Success": "True", "Report": data[0]}),
+                data=json.dumps(dict(address=data[3], success=True, report=data[0])),
             )
 
         except Exception:
